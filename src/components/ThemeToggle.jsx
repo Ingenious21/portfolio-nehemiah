a8b1c4393892fc4e58dc9bc4cff966e735c5d479
Nehemiah Kemayah <ingeniouskemayah@gmail.com>
2025-09-02 00:21:14 +0000

    toggle fixed for smaller devices

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,15 +32,15 @@ export const ThemeToggle = () => {
 
     return (
     <button onClick={toggleTheme} className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-hidden"
+        "fixed top-3 right-3 sm:top-5 sm:right-5 z-50 p-1.5 sm:p-2 rounded-full transition-colors duration-300",
+        "focus:outline-none hover:bg-primary/10"
         
     )}
     > 
         { isDarkMode ? (
-            <Sun className="h-6 w-6 text-yellow-300" /> 
+            <Sun className="h-5 w-5 sm:h-6 sm:w-6 text-yellow-300" /> 
         ) : ( 
-            <Moon className="h-6 w-6 text-blue-900" />
+            <Moon className="h-5 w-5 sm:h-6 sm:w-6 text-blue-900" />
         )}
         </button>
     );
